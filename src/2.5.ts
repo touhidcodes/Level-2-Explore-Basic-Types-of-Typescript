8ca43d8cfd93ddb0453306e45e297d1457158af9
touhidcodes <touhidcodes@gmail.com>
2023-11-02 00:10:54 +0600

    object type

---
+++ src/2.5.ts
@@ -0,0 +1,16 @@
+// Reference Type --> Object
+
+const user: {
+  readonly country: string; // access modifier (value is fixed)
+  company: "Programming Hero"; // literal type (fixed)
+  firstName: string;
+  middleName?: string; // optional type (?)
+  lastName: string;
+  isMarried: boolean;
+} = {
+  country: "Bangladesh",
+  company: "Programming Hero",
+  firstName: "Touhidur",
+  lastName: "Zaman",
+  isMarried: true,
+};
