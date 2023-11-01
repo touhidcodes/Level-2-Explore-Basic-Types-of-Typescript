8fee352b62a9c2f0a6aa361886f6e53fb5fdfc3e
touhidcodes <touhidcodes@gmail.com>
2023-11-02 00:11:56 +0600

    object type

---
+++ dist/object-data-types.js
@@ -0,0 +1,9 @@
+"use strict";
+// Reference Type --> Object
+const user = {
+    country: "Bangladesh",
+    company: "Programming Hero",
+    firstName: "Touhidur",
+    lastName: "Zaman",
+    isMarried: true,
+};
